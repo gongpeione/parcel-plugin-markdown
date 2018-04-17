ff5529d85a067c951a0386fb632a0fdcc566caef
Jason Yu <me@ycmjason.com>
2018-04-17 15:07:10 +0100

    removed dependency of posthtml-parser by using HTMLAsset.generate(), also fixing tests

--- test/test.js
+++ test/test.js
@@ -5,15 +5,16 @@ const path = require('path');
 describe('MarkdownAsset', function() {
     it('should parse markdown correctly.', async function() {
         const result = 'module.exports=`<h1 id="test">Test</h1>\n<h2 id="h2">H2</h2>\n`';
-        const instance = new mdasset(path.resolve(__dirname, './index.md'), {}, {});
+        const instance = new mdasset(path.resolve(__dirname, './index.md'), {}, { rootDir: __dirname });
         const processed = await instance.process();
         
         assert.equal(processed.js, result);
     });
     it('should parse markdown with images correctly.', async function() {
-        const result = 'module.exports=`<h1 id="test">Test</h1>\n<h2 id="h2">H2</h2>\n<p><img src="/2fdf41fd143f7af67e0a6b4c11576a45.jpg" alt=""></p>\n`';
+        const result = 'module.exports=`<h1 id="test">Test</h1>\n<h2 id="h2">H2</h2>\n<p><img src="/3fd20fc0b104a3e28c22e1fd7d5d4a68.jpg" alt=""></p>\n`';
         const Parser = require('parcel-bundler/src/Parser');
         const instance = new mdasset(path.resolve(__dirname, './index.img.md'), {}, { 
+            rootDir: __dirname,
             parser: new Parser(),
             publicURL: '/' 
         });
