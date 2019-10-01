0e9fef85bffb55bccb2b198b9f661459a2aae04b
Clemens Damke <admin@cortys.de>
2019-10-01 21:35:18 +0200

    added support for renderer method overrides

--- MarkdownAsset.js
+++ MarkdownAsset.js
@@ -4,7 +4,8 @@ const marked = require('marked');
 module.exports = class MarkdownAsset extends HTMLAsset {
   async parse (code) {
     const pkg = await this.getPackage();
-    const config = await this.getConfig(['marked.config.js']);
+    const configPath = await this.getConfig(['marked.config.js'], { load: false });
+    const config = require(configPath); // Don't clone the config to allow for renderer overrides.
     this.markedOptions = config || pkg.marked || {};
     this.contents = marked(code, this.markedOptions);
 
